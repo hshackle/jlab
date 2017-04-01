05911d0e323833a10f8db6131dd24f2594ab7241
Henry <hshackle@mit.edu>
2017-04-01 17:21:10 +0000

    fitting

--- dynamics/inhomo_measure.C
+++ dynamics/inhomo_measure.C
@@ -1,9 +1,9 @@
-void inhomo() {
+void inhomo_measure() {
   TCanvas *c1 = new TCanvas("c1", "Inhomogeneity Fit", 200, 10, 700 ,500);
-  Double_t y[4] = {112.3, 103.5, 93.6, 84.5};
-  Double_t x[4]= { 112.8, 103.8, 94.0, 84.6};
-  Double_t xerr[4] = {1.2, 1.1, 1.0, .9};
-  Double_t yerr[4] ={.7, .6, .6, .5};
+  Double_t y[4] = {119.5,110.1,99.6, 89.9};
+  Double_t x[4]= {120, 110.4, 100, 90};
+  Double_t xerr[4] = {.2, .2, .2,.2};
+  Double_t yerr[4] ={.07, .07, .07, .07};
 
   gr = new TGraphErrors(4, x, y, xerr, yerr);
   gr->Draw("AP");
@@ -13,5 +13,5 @@ void inhomo() {
 };
 
 int main(){
-  inhomo();
+  inhomo_measure();
 }
