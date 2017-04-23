62ee3b2e43225b9c4a97a29a4b222b73d578acf2
hshackle <hshackle@mit.edu>
2017-04-23 01:32:02 -0400

    ruth

--- rutherford/convolve.py
+++ rutherford/convolve.py
@@ -8,7 +8,7 @@ from scipy.odr import ODR, Model, Data, RealData
 
 
 def convolve_func(theta,phi):
-    return 1/numpy.power(numpy.sin(theta/2), 4)*(1-numpy.absolute(theta-phi)/.13) *0.5 * (numpy.sign(-numpy.absolute(theta-phi) + .13) + 1)
+    return 1/numpy.power(numpy.sin(theta/2), 4)*(1-numpy.absolute(theta-phi)/.122) *0.5 * (numpy.sign(-numpy.absolute(theta-phi) + .122) + 1)
 #* Heaviside(-numpy.absolute(theta-phi) + .122)
 
 def convolve(phi):
@@ -21,16 +21,20 @@ def convolve(phi):
 def fitFunc(A, x):
     return A[0]*convolve(x)
 
+def nonConv(A, x):
+    return A[0]/numpy.power(numpy.sin(x/2), 4)
 
-"""xdata = numpy.array([.174, .2618, .3491, .5236, .6981, .8727, 1.047])
+
+xdata = numpy.array([.174, .2618, .3491, .5236, .6981, .8727, 1.047])
+xdata = xdata - 0.0314
 ydata = numpy.array([9.64, 0.66, 0.12, 0.02, 0.005, 0.002156, 0.0009])
 x_err = numpy.array([0.0087, 0.0087, 0.0087, 0.0087, 0.0087, 0.0087, 0.0087])
-y_err = numpy.array([1.79, 0.3, 0.005, 0.00049, 0.000253, 0.00009, 0.00004])"""
-xdata = numpy.array([.3491, .5236, .6981, .8727, 1.047])
+y_err = numpy.array([1.79, 0.3, 0.005, 0.00049, 0.000253, 0.00009, 0.00004])
+"""xdata = numpy.array([.3491, .5236, .6981, .8727, 1.047])
 ydata = numpy.array([ 0.12, 0.02, 0.005, 0.002156, 0.0009])
-x_err = numpy.array([ 0.0087, 0.0087, 0.0087, 0.0087, 0.0087])
-x_err = x_err*2
-y_err = numpy.array([0.005, 0.00049, 0.000253, 0.00009, 0.00004])
+x_err = numpy.array([ 0.0087, 0.0087, 0.0087, 0.0087, 0.0087])"""
+#x_err = x_err*2
+#y_err = numpy.array([0.005, 0.00049, 0.000253, 0.00009, 0.00004])
 conv_model = Model(fitFunc)
 data = RealData(xdata,ydata, sx=x_err, sy=y_err)
 odr = ODR(data, conv_model, beta0=[0.005])
@@ -38,11 +42,21 @@ odr = ODR(data, conv_model, beta0=[0.005])
 out = odr.run()
 out.pprint()
 
-x_fit = numpy.linspace(xdata[0] - 0.05, xdata[-1]+ .05, 1000)
+nonconv_model = Model(nonConv)
+nondata = RealData(xdata,ydata, sx=x_err, sy=y_err)
+nonodr = ODR(nondata, nonconv_model, beta0=[0.005])
+
+nonout = nonodr.run()
+nonout.pprint()
+
+
+x_fit = numpy.linspace(xdata[0] - 0.01, xdata[-1]+ .01, 1000)
 y_fit = fitFunc(out.beta, x_fit)
+nony_fit = nonConv(nonout.beta, x_fit)
 
 plt.errorbar(xdata*57.296, ydata, xerr=x_err*57.296, yerr=y_err, linestyle='None',ecolor='b', marker='None')
 plt.semilogy(x_fit*57.296, y_fit)
+plt.semilogy(x_fit*57.296, nony_fit)
 
 plt.show()
 
