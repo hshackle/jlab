1eed03db1dec8f6d13df9164db18bd3b778664e0
Henry <hshackle@mit.edu>
2017-04-24 11:23:23 +0000

    presentation

--- rutherford/convolve.py
+++ rutherford/convolve.py
@@ -24,6 +24,22 @@ def fitFunc(A, x):
 def nonConv(A, x):
     return A[0]/numpy.power(numpy.sin(x/2), 4)
 
+def plum_convolve_func(theta,phi):
+    return numpy.exp(numpy.divide(-numpy.power(theta,2),numpy.power(1.0, 2)))*(1-numpy.absolute(theta-phi)/.122) *0.5 * (numpy.sign(-numpy.absolute(theta-phi) + .122) + 1)
+#* Heaviside(-numpy.absolute(theta-phi) + .122)
+
+def plum_convolve(phi):
+    plum_sol = []
+    for i in phi:
+        plum_sol.append(integrate.quad(lambda x: plum_convolve_func(x, i), 0.0, 3.14159)[0])
+    #result = integrate.quad(lambda x: convolve_func(x, phi), 0.01, 3.14159)
+    return numpy.array(plum_sol)
+
+def plum_fitFunc(A, x):
+    return A[0]*plum_convolve(x)
+
+def plum_nonConv(A, x):
+    return A[0]*numpy.exp(-numpy.power(x,2)/numpy.power(0.017453,2))
 
 xdata = numpy.array([.174, .2618, .3491, .5236, .6981, .8727, 1.047])
 xdata = xdata - 0.0314
@@ -42,6 +58,14 @@ odr = ODR(data, conv_model, beta0=[0.005])
 out = odr.run()
 out.pprint()
 
+plum_conv_model = Model(plum_fitFunc)
+plum_data = RealData(xdata,ydata, sx=x_err, sy=y_err)
+plum_odr = ODR(plum_data, plum_conv_model, beta0=[0.05])
+
+plum_out = plum_odr.run()
+plum_out.pprint()
+
+
 nonconv_model = Model(nonConv)
 nondata = RealData(xdata,ydata, sx=x_err, sy=y_err)
 nonodr = ODR(nondata, nonconv_model, beta0=[0.005])
@@ -50,13 +74,29 @@ nonout = nonodr.run()
 nonout.pprint()
 
 
+plum_nonconv_model = Model(plum_nonConv)
+plum_nondata = RealData(xdata,ydata, sx=x_err, sy=y_err)
+plum_nonodr = ODR(plum_nondata, plum_nonconv_model, beta0=[0.005])
+
+plum_nonout = plum_nonodr.run()
+plum_nonout.pprint()
+
 x_fit = numpy.linspace(xdata[0] - 0.01, xdata[-1]+ .01, 1000)
 y_fit = fitFunc(out.beta, x_fit)
+plum_y_fit = plum_fitFunc(plum_out.beta, x_fit)
 nony_fit = nonConv(nonout.beta, x_fit)
+plum_nony_fit = plum_nonConv(plum_nonout.beta, x_fit)
 
 plt.errorbar(xdata*57.296, ydata, xerr=x_err*57.296, yerr=y_err, linestyle='None',ecolor='b', marker='None')
-plt.semilogy(x_fit*57.296, y_fit)
-plt.semilogy(x_fit*57.296, nony_fit)
+plt.plot(x_fit*57.296, y_fit)
+#plt.plot(x_fit*57.296, y_fit)
+plt.plot(x_fit*57.296, plum_y_fit)
+#plt.semilogy(x_fit*57.296, plum_nony_fit)
+#plt.plot(x_fit*57.296, plum_nony_fit)
+#plt.semilogy(x_fit*57.296, nony_fit)
+
+axes=plt.gca()
+axes.set_ylim([.001, 100])
 
 plt.show()
 
