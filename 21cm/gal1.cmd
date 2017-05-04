eac20bcfd071fe1e02fcb6259fd64cdc29162b76
Henry <hshackle@mit.edu>
2017-05-03 23:10:37 -0400

    repreat script

--- 21cm/gal1.cmd
+++ 21cm/gal1.cmd
@@ -1,37 +1,28 @@
-: galactic 170 0
-: record g170.rad 	
-: repeat 5
+: freq 1420.4 4 	
+: azel 240 70 	
+: noisecal 
+: galactic 170 0 	
+: record g170.rad
 :60
 : roff
-: galactic 180 0
+: galactic 180 0 	
 : record g180.rad 	
-: repeat 5
 :60
 : roff
 : galactic 190 0
 : record g190.rad 	
-: repeat 5
 :60
 : roff
 : galactic 200 0
 : record g200.rad 	
-: repeat 5
 :60
 : roff
 : galactic 210 0
 : record g210.rad 	
-: repeat 5
 :60
 : roff
 : galactic 220 0
 : record g220.rad 	
-: repeat 5
 :60
 : roff
-: galactic 230 0
-: record g230.rad 	
-: repeat 5
-:60
-: roff
-:
-
+: 	
